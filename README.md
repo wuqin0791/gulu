372f4df363f2c30460e438913f91472d458cf5cb
jeannewu <454106678@qq.com>
2020-06-23 23:22:48 +0800

    button1.0提交

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # gulu
+
+## 安装
+> 使用本框架前要使用border-box
+```
+box-sizing: border-box
+```
---
+++ button.js
@@ -0,0 +1,13 @@
+
+
+/*
+ * @Description: This is a XX file
+ * @Author: JeanneWu
+ * @Date: 2020-06-23 15:11:00
+ */ 
+// js文件头部注释之后的内容
+Vue.component('g-button',{
+  template: `
+    <button class="g-button">Hi</button>
+  `
+})
