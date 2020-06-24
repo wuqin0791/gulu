f4b8ba33b5077b400b42145cba658743a5a5e77f
jeannewu <454106678@qq.com>
2020-06-24 12:28:06 +0800

    加入parcel打包

---
+++ src/button.vue
@@ -0,0 +1,40 @@
+
+
+<!--
+ * @Description: This is a XX file
+ * @Author: JeanneWu
+ * @Date: 2020-06-24 11:28:57
+--> 
+
+
+<template>
+  <button class="g-button">Hi</button>
+</template>
+<script type="text/ecmascript-6">
+export default {
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
+<style scoped lang="scss">
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+}
+</style>
